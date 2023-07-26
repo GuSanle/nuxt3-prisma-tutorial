a4217de30bab40853ab684d0a6940a4bb7a8fb20
slgu <slgu@cybozu.cn>
2023-07-26 18:18:39 +0800

    加入env的类型声明
    加入env的类型声明

---
+++ types/environment.d.ts
@@ -0,0 +1,12 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NUXT_AUTH_SECRET: string;
+      NODE_ENV: string;
+      DATABASE_URL: string;
+      KINTONE_API_TOKEN: string;
+    }
+  }
+}
